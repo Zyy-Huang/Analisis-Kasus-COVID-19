07f02bb986a4533f1c7428b0ebef917d71a7cc7f
Mohammad Ilham Fauzy <fauzy155379@gmail.com>
2025-01-31 09:08:52 +0700

    Update covid.py

--- covid.py
+++ covid.py
@@ -36,8 +36,8 @@ def main():
     # Judul aplikasi 
     '''
     # :syringe: Analisis Kasus COVID-19
-    st.title("Kelompok-6")
-    st.markdown("Aplikasi ini menampilkan data kasus COVID-19 berdasarkan analisis yang mendalam.")
+    Kelompok-6
+    Aplikasi ini menampilkan data kasus COVID-19 berdasarkan analisis yang mendalam.
     '''
     # Cek file yang tersedia dalam direktori saat ini dan dalam folder dataset
     available_files = os.listdir(".")
