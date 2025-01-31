47eaef0c52f1cc6d354b3ba7d101b8f63b6ee87f
Mohammad Ilham Fauzy <fauzy155379@gmail.com>
2025-01-31 09:33:58 +0700

    Update covid.py

--- covid.py
+++ covid.py
@@ -34,7 +34,7 @@ def gompertz(a, c, t, t_0):
 # Fungsi utama untuk aplikasi Streamlit
 def main():
     # Judul aplikasi 
-    st.title(":syringe: Analisis Kasus COVID-19",color="#98FB98")
+    st.title(":syringe: :Green[Analisis Kasus COVID-19]")
     st.title("Kelompok-6:")
     st.markdown("Mohammad Ilham Fauzy")
     st.markdown("Nabila Zahra Alia")
